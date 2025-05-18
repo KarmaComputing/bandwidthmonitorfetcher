73ef2f4d195e67aad09d18f1c2fa94b2cc4f1dd0
chrisjsimpson <chris.j.simpson@live.co.uk>
2025-05-18 20:18:03 +0100

    oh that's nice, using debug println deriving debug trait

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 use reqwest::{self};
+use serde::Deserialize;
 use std::error::Error;
-use types::VnStats;
 
-mod types;
+#[derive(Deserialize, Debug)]
+struct VnStats {
+    vnstatversion: String,
+    jsonversion: String,
+    interfaces: Vec<VnStatInterface>,
+}
+
+#[derive(Deserialize, Debug)]
+struct VnStatInterface {
+    name: String,
+    alias: String,
+    created: VnStatDateCreated,
+}
+
+#[derive(Deserialize, Debug)]
+struct VnStatDateCreated {
+    date: VnStatDate,
+    timestamp: u32,
+}
+
+#[derive(Deserialize, Debug)]
+struct VnStatDate {
+    day: u32,
+    month: u32,
+    year: u32,
+}
 
 fn main() {
     run().unwrap()
@@ -14,10 +39,21 @@ fn run() -> Result<(), Box<dyn Error>> {
     println!("{req:?}");
     println!("The vnstatversion is: {}", req.vnstatversion);
     println!("The jsonversion is: {}", req.jsonversion);
+    let num_interfaces = req.interfaces.len();
+    println!("The number of interfaces is: {}", num_interfaces);
+    println!("{req:#?}");
+    /*
     println!(
-        "An interface {:?}, name: {}, alias: {}",
-        req.interfaces[0], req.interfaces[0].name, req.interfaces[0].alias
+        "An interface {:?}\n, name: {}\n, alias: {}\n. Created: {}/{}/{}\n Timestamp: {} \n. ",
+        req.interfaces[0],
+        req.interfaces[0].name,
+        req.interfaces[0].alias,
+        req.interfaces[0].created.date.day,
+        req.interfaces[0].created.date.month,
+        req.interfaces[0].created.date.year,
+        req.interfaces[0].created.timestamp
     );
+    */
 
     Ok(())
 }
