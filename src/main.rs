7ce0d0f5a200aa9396d9a8f9321bf71f9eea0075
chrisjsimpson <chris.j.simpson@live.co.uk>
2025-05-18 17:40:04 +0100

    added more vnstat struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::{error::Error};
 use reqwest::{self};
 use serde::Deserialize;
-
+use std::error::Error;
 
 #[derive(Deserialize, Debug)]
 struct VnStats {
     vnstatversion: String,
     jsonversion: String,
+    interfaces: Vec<VnStatInterface>,
+}
+
+#[derive(Deserialize, Debug)]
+struct VnStatInterface {
+    name: String,
+    alias: String,
 }
 
 fn main() {
@@ -14,20 +20,21 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
+    let req: VnStats = reqwest::blocking::get("http://127.0.0.1:8685/json.cgi")?.json()?;
 
-    let req: VnStats = reqwest::blocking::get("http://127.0.0.1:8685/json.cgi")?
-    .json()?;
-    
     println!("{req:?}");
     println!("The vnstatversion is: {}", req.vnstatversion);
     println!("The jsonversion is: {}", req.jsonversion);
+    println!(
+        "An interface {:?}, name: {}, alias: {}",
+        req.interfaces[0], req.interfaces[0].name, req.interfaces[0].alias
+    );
 
     Ok(())
 }
 
-
 /*
-    // The question operator is a shorthand for the match Ok/Err verbose approach (which is useful) but 
+    // The question operator is a shorthand for the match Ok/Err verbose approach (which is useful) but
     // not always needed (too noisy!)
 
     let contents = match req {
@@ -36,5 +43,3 @@ fn run() -> Result<(), Box<dyn Error>> {
     }
 
 */
-
-
