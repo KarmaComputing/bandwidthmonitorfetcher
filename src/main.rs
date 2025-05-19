bdcefac156368633e6ec99b920802d80e6aa8324
chrisjsimpson <chris.j.simpson@live.co.uk>
2025-05-19 12:23:46 +0100

    implement get_interface

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct VnStats {
     interfaces: Vec<VnStatInterface>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatInterface {
     name: String,
     alias: String,
@@ -18,19 +18,19 @@ struct VnStatInterface {
     traffic: VnStatTraffic,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatTraffic {
     total: VnStatTrafficTotal,
     fiveminute: Vec<VnStatTrafficFiveminute>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatTrafficTotal {
     rx: u64,
     tx: u64,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatTrafficFiveminute {
     id: u32,
     date: VnStatDate,
@@ -40,26 +40,26 @@ struct VnStatTrafficFiveminute {
     tx: u64,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatDateCreated {
     date: VnStatDate,
     timestamp: u32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatDateUpdated {
     date: VnStatDate,
     time: VnStatTime,
     timestamp: u32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatTime {
     hour: u32,
     minute: u32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct VnStatDate {
     day: u32,
     month: u32,
@@ -77,6 +77,22 @@ impl VnStats {
 
         VnStatTrafficTotal { tx, rx }
     }
+
+    fn get_interface(interface_name: &str) -> Result<Option<VnStatInterface>, Box<dyn Error>> {
+        // Locate a given interface from the vnstats json output
+        let vnstats: VnStats = reqwest::blocking::get("http://127.0.0.1:8685/json.cgi")?.json()?;
+
+        println!("Getting interface_name: {}", interface_name);
+        let target_interface = "wlp0s20f3";
+
+        let interface = vnstats
+            .interfaces
+            .iter()
+            .find(|interface| interface.name == target_interface)
+            .cloned();
+
+        Ok(interface)
+    }
 }
 
 fn main() {
@@ -84,16 +100,40 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let req: VnStats = reqwest::blocking::get("http://127.0.0.1:8685/json.cgi")?.json()?;
+    let vnstats: Result<Option<VnStatInterface>, Box<dyn Error + 'static>> =
+        VnStats::get_interface("eth0");
 
+    match vnstats {
+        Ok(Some(interface)) => {
+            println!("Traffic: {:?}", interface.traffic);
+        }
+        Ok(None) => {
+            println!("No interface found with that name.");
+        }
+        Err(e) => {
+            eprintln!("Error getting interface traffic: {}", e);
+        }
+    }
+
+    /*
+     To handle:
+     Result, could be an error
+     Inside the Result is an Option, which could be None.
+    */
+
+    /*
     println!("{req:?}");
+
+
     println!("The vnstatversion is: {}", req.vnstatversion);
     println!("The jsonversion is: {}", req.jsonversion);
     let num_interfaces = req.interfaces.len();
     println!("The number of interfaces is: {}", num_interfaces);
-    println!("{req:#?}");
+    //println!("{req:#?}");
     let total = req.fiveminute_total();
     println!("fiveminuteTotal: {:?}", { total });
+    */
+
     /*
     println!(
         "An interface {:?}\n, name: {}\n, alias: {}\n. Created: {}/{}/{}\n Timestamp: {} \n. ",
